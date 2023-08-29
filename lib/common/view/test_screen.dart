512a5bb4f158c83e15ff3870cd6401cb232bbbc9
dddog <azskw1101@gmail.com>
2023-08-29 14:54:03 +0900

    다국어 셋팅 Fixes #4

---
+++ lib/common/view/test_screen.dart
@@ -0,0 +1,23 @@
+import 'package:biskit_app/common/layout/default_layout.dart';
+import 'package:biskit_app/common/utils/logger_util.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    logger.d(context.locale.toString());
+    return DefaultLayout(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('hello').tr(),
+          ],
+        ),
+      ),
+    );
+  }
+}
