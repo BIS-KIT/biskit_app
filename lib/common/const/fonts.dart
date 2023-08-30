d50d89b5eeceaa775dc1f31efe220c5c06511a0b
dddog <azskw1101@gmail.com>
2023-08-30 16:39:19 +0900

    폰트 추가 Fixes #13

---
+++ lib/common/const/fonts.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+// 국문
+const TextStyle kTsKrHeading24Bd = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  height: 1.4,
+);
+
+const TextStyle kTsKrHeading20Bd = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  height: 1.4,
+);
+
+const TextStyle kTsKrHeading18Bd = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  height: 1.4,
+);
+
+const TextStyle kTsKrBody16Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  height: 1.5,
+);
+
+const TextStyle kTsKrBody16Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
+);
+
+const TextStyle kTsKrBody14Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  height: 1.5,
+);
+
+const TextStyle kTsKrBody14Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
+);
+
+const TextStyle kTsKrBody12Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  height: 1.5,
+);
+
+const TextStyle kTsKrBody12Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
+);
+
+const TextStyle kTsKrCaption11Md = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+  height: 1.5,
+);
+
+// 영문
+const TextStyle kTsEnHeading24Eb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 24,
+  fontWeight: FontWeight.w800,
+  height: 1.3,
+  letterSpacing: -0.24,
+);
+
+const TextStyle kTsEnHeading20Eb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 20,
+  fontWeight: FontWeight.w800,
+  height: 1.3,
+  letterSpacing: -0.20,
+);
+
+const TextStyle kTsEnHeading18Eb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Suite',
+  fontSize: 18,
+  fontWeight: FontWeight.w800,
+  height: 1.3,
+  letterSpacing: -0.18,
+);
+
+const TextStyle kTsEnBody16Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  height: 1.4,
+);
+
+const TextStyle kTsEnBody16Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  height: 1.4,
+);
+
+const TextStyle kTsEnBody14Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  height: 1.4,
+);
+
+const TextStyle kTsEnBody14Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  height: 1.4,
+);
+
+const TextStyle kTsEnBody12Sb = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  height: 1.4,
+);
+
+const TextStyle kTsEnBody12Rg = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  height: 1.4,
+);
+
+const TextStyle kTsEnCaption11Md = TextStyle(
+  color: Color(0xFF292C45),
+  fontFamily: 'Pretendard',
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+  height: 1.4,
+);
