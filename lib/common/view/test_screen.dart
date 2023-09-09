e1d46347d25fc6a20ac481e3ec30f4f593b7cc93
dddog <azskw1101@gmail.com>
2023-09-09 17:58:19 +0900

    fix

--- lib/common/view/test_screen.dart
+++ lib/common/view/test_screen.dart
@@ -6,6 +6,7 @@ import 'package:biskit_app/common/utils/logger_util.dart';
 import 'package:biskit_app/common/view/photo_manager_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart' as kakao;
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
@@ -23,12 +24,27 @@ class _TestScreenState extends State<TestScreen> {
     if (await kakao.isKakaoTalkInstalled()) {
       try {
         await kakao.UserApi.instance.loginWithKakaoTalk();
+        logger.d('카카오톡으로 로그인 성공');
       } catch (e) {
         logger.e(e);
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (e is PlatformException && e.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await kakao.UserApi.instance.loginWithKakaoAccount();
+          logger.d('카카오계정으로 로그인 성공');
+        } catch (error) {
+          logger.d('카카오계정으로 로그인 실패 $error');
+        }
       }
     } else {
       try {
         await kakao.UserApi.instance.loginWithKakaoAccount();
+        logger.d('카카오계정으로 로그인 성공');
         kakao.User user = await kakao.UserApi.instance.me();
         logger.d('사용자 정보 요청 성공'
             '\n회원번호: ${user.id}'
