d888765c35e553f8c5c290f4ffec2ca9dae1b8ab
김유나 <yuna000804@gmail.com>
2023-09-16 19:30:21 +0900

    feat: input error case 추가 및 focusNode 설정

--- lib/common/view/name_birth_gender_screen.dart
+++ lib/common/view/name_birth_gender_screen.dart
@@ -6,6 +6,7 @@ import 'package:biskit_app/common/component/text_input_widget.dart';
 import 'package:biskit_app/common/const/colors.dart';
 import 'package:biskit_app/common/const/fonts.dart';
 import 'package:biskit_app/common/layout/default_layout.dart';
+import 'package:biskit_app/common/view/single_national_flag_screen%20copy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -23,11 +24,54 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
   String birthMonth = '';
   String birthDay = '';
   String? nameError;
-  SelectWidgetValueType selectWidgetValueType = SelectWidgetValueType.none;
+  String? birthYearError;
+  String? birthMonthError;
+  String? birthDayError;
+  bool isValidName = false;
+  bool isValidBirth = false;
+  bool isValidGender = false;
+  late FocusNode birthMonthFocusNode;
+  late FocusNode birthDayFocusNode;
+  late FocusNode nameFocusNode;
+  SelectWidgetValueType selectGender = SelectWidgetValueType.none;
 
-  bool isValidName(String name) {
+  @override
+  void initState() {
+    super.initState();
+    nameFocusNode = FocusNode();
+    birthMonthFocusNode = FocusNode();
+    birthDayFocusNode = FocusNode();
+    nameFocusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameFocusNode.dispose();
+    birthMonthFocusNode.dispose();
+    birthDayFocusNode.dispose();
+  }
+
+  bool isValidNameType(String name) {
     RegExp koreanEnglishRegExp = RegExp(r'^[a-zA-Zㄱ-ㅎ가-힣]+$');
     return koreanEnglishRegExp.hasMatch(name);
+  }
+
+  checkIsValidForm() {
+    return isValidName && isValidGender;
+  }
+
+  checkIsValidBirth() {
+    if (birthYear.isNotEmpty &&
+        birthYearError == null &&
+        birthMonth.isNotEmpty &&
+        birthMonthError == null &&
+        birthDay.isNotEmpty &&
+        birthDayError == null) {
+      setState(() {
+        isValidBirth = true;
+      });
+    }
   }
 
   checkName() {
@@ -35,14 +79,83 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
       setState(() {
         nameError = null;
       });
-    } else if (!isValidName(name)) {
+    } else if (!isValidNameType(name)) {
       setState(() {
         nameError = '한글 또는 영문으로 입력해주세요';
       });
     } else {
       setState(() {
         nameError = null;
+        isValidName = true;
       });
+    }
+  }
+
+  checkYear() {
+    if (birthYear.length > 1 && int.parse(birthYear.substring(0, 2)) < 19) {
+      setState(() {
+        birthYearError = '';
+      });
+    } else if (birthYear.length == 4) {
+      birthYearError = null;
+      FocusScope.of(context).requestFocus(birthMonthFocusNode);
+    } else {
+      setState(() {
+        birthYearError = null;
+      });
+    }
+  }
+
+  checkMonth() {
+    if (birthMonth.length > 1 &&
+        (int.parse(birthMonth) < 01 || int.parse(birthMonth) > 12)) {
+      setState(() {
+        birthMonthError = '';
+      });
+    } else if (birthMonth.length == 2) {
+      birthMonthError = null;
+      FocusScope.of(context).requestFocus(birthDayFocusNode);
+    } else {
+      setState(() {
+        birthMonthError = null;
+      });
+    }
+  }
+
+  checkDay() {
+    if (birthMonth == '02' && birthDay == '30') {
+      birthDayError = '';
+    } else if (birthDay == '31' &&
+        (birthMonth == '02' ||
+            birthMonth == '04' ||
+            birthMonth == '06' ||
+            birthMonth == '08' ||
+            birthMonth == '09' ||
+            birthMonth == '11')) {
+      birthDayError = '';
+    } else if (birthDay.length > 1 &&
+        (int.parse(birthDay) < 1 || int.parse(birthDay) > 31)) {
+      setState(() {
+        birthDayError = '';
+      });
+    } else {
+      birthDayError = null;
+    }
+  }
+
+  checkDate() {
+    if (birthMonth == '02' && birthDay == '30') {
+      birthDayError = '';
+    } else if (birthDay == '31' &&
+        (birthMonth == '02' ||
+            birthMonth == '04' ||
+            birthMonth == '06' ||
+            birthMonth == '08' ||
+            birthMonth == '09' ||
+            birthMonth == '11')) {
+      birthDayError = '';
+    } else {
+      birthDayError = null;
     }
   }
 
@@ -69,6 +182,7 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
                 keyboardType: TextInputType.name,
                 errorText: nameError,
                 textInputAction: TextInputAction.next,
+                focusNode: nameFocusNode,
                 onChanged: (value) {
                   name = value;
                   checkName();
@@ -94,21 +208,32 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Flexible(
-                          child: CustomTextFormField(
-                        textAlign: TextAlign.center,
-                        maxLength: 4,
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
-                        hintText: 'YYYY',
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            birthYear = value;
-                          });
-                        },
-                      )),
+                        child: Focus(
+                            onFocusChange: (value) {
+                              if (!value && birthYear.length != 4) {
+                                setState(() {
+                                  birthYearError = '';
+                                });
+                              }
+                            },
+                            child: CustomTextFormField(
+                              textAlign: TextAlign.center,
+                              maxLength: 4,
+                              errorText: birthYearError,
+                              keyboardType: TextInputType.number,
+                              textInputAction: TextInputAction.next,
+                              hintText: 'YYYY',
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              onChanged: (value) {
+                                setState(() {
+                                  birthYear = value;
+                                });
+                                checkYear();
+                              },
+                            )),
+                      ),
                       const SizedBox(
                         width: 8,
                       ),
@@ -116,40 +241,73 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
                         child: Row(
                           children: [
                             Flexible(
-                                child: CustomTextFormField(
-                              textAlign: TextAlign.center,
-                              maxLength: 2,
-                              keyboardType: TextInputType.number,
-                              textInputAction: TextInputAction.next,
-                              hintText: 'MM',
-                              inputFormatters: [
-                                FilteringTextInputFormatter.digitsOnly
-                              ],
-                              onChanged: (value) {
-                                setState(() {
-                                  birthMonth = value;
-                                });
-                              },
-                            )),
+                              child: Focus(
+                                  onFocusChange: (value) {
+                                    if (!value &&
+                                        birthMonth.isNotEmpty &&
+                                        birthMonth.length != 2) {
+                                      setState(() {
+                                        birthMonthError = '';
+                                      });
+                                    }
+                                  },
+                                  child: CustomTextFormField(
+                                    textAlign: TextAlign.center,
+                                    maxLength: 2,
+                                    keyboardType: TextInputType.number,
+                                    textInputAction: TextInputAction.next,
+                                    hintText: 'MM',
+                                    readOnly: birthYear.isEmpty ||
+                                        birthYearError != null,
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
+                                    errorText: birthMonthError,
+                                    focusNode: birthMonthFocusNode,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        birthMonth = value;
+                                      });
+                                      checkMonth();
+                                      checkDate();
+                                    },
+                                  )),
+                            ),
                             const SizedBox(
                               width: 8,
                             ),
                             Flexible(
-                                child: CustomTextFormField(
-                              textAlign: TextAlign.center,
-                              maxLength: 2,
-                              keyboardType: TextInputType.number,
-                              textInputAction: TextInputAction.next,
-                              hintText: 'DD',
-                              inputFormatters: [
-                                FilteringTextInputFormatter.digitsOnly
-                              ],
-                              onChanged: (value) {
-                                setState(() {
-                                  birthDay = value;
-                                });
-                              },
-                            )),
+                              child: Focus(
+                                  onFocusChange: (value) {
+                                    if (!value &&
+                                        birthDay.isNotEmpty &&
+                                        birthDay.length != 2) {
+                                      setState(() {
+                                        birthDayError = '';
+                                      });
+                                    }
+                                  },
+                                  child: CustomTextFormField(
+                                    textAlign: TextAlign.center,
+                                    maxLength: 2,
+                                    keyboardType: TextInputType.number,
+                                    textInputAction: TextInputAction.next,
+                                    hintText: 'DD',
+                                    readOnly: birthMonth.isEmpty ||
+                                        birthMonthError != null,
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
+                                    errorText: birthDayError,
+                                    focusNode: birthDayFocusNode,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        birthDay = value;
+                                      });
+                                      checkDay();
+                                    },
+                                  )),
+                            ),
                           ],
                         ),
                       ),
@@ -175,15 +333,17 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
                   SelectWidget(
                       leftText: '여성',
                       rightText: '남성',
-                      value: selectWidgetValueType,
+                      value: selectGender,
                       onTapLeft: () {
                         setState(() {
-                          selectWidgetValueType = SelectWidgetValueType.left;
+                          selectGender = SelectWidgetValueType.left;
+                          isValidGender = true;
                         });
                       },
                       onTapRight: () {
                         setState(() {
-                          selectWidgetValueType = SelectWidgetValueType.right;
+                          selectGender = SelectWidgetValueType.right;
+                          isValidGender = true;
                         });
                       })
                 ],
@@ -194,11 +354,22 @@ class _NameBirthGenderScreenState extends State<NameBirthGenderScreen> {
             padding: const EdgeInsets.only(bottom: 0),
             child: GestureDetector(
               onTap: () {
-                // context.pushReplacementNamed()
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SingleNationalFlagScreen(),
+                    ));
               },
-              child: const FullBleedButtonWidget(
+              child: FullBleedButtonWidget(
                 text: '다음',
-                isEnable: false,
+                isEnable: isValidName &&
+                    birthYear.isNotEmpty &&
+                    birthYearError == null &&
+                    birthMonth.isNotEmpty &&
+                    birthMonthError == null &&
+                    birthDay.isNotEmpty &&
+                    birthDayError == null &&
+                    isValidGender,
               ),
             ),
           )
