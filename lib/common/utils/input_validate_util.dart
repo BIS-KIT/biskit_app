9cabed2027d6378302eabc7108a45356f0dc2de5
dddog <azskw1101@gmail.com>
2023-09-19 10:17:26 +0900

    fix/닉네임 입력시 디바운스 액션 처리 Fixes #91

--- lib/common/utils/input_validate_util.dart
+++ lib/common/utils/input_validate_util.dart
@@ -5,12 +5,17 @@ extension InputValidate on String {
             r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
         .hasMatch(this);
   }
-}
 
-bool isValidPassword(String password) {
-  // 영문자, 숫자, 특수문자 중 하나 이상 포함, 총 길이는 8 이상 16 이하
-  final RegExp pattern = RegExp(r'^(?=.*?[a-zA-Z0-9!@#\$&*~]).{8,16}$');
+  /// 비밀번호 포맷 검증 영문자, 숫자, 특수문자 중 하나 이상 포함, 총 길이는 8 이상 16 이하
+  bool isValidPassword() {
+    final RegExp pattern = RegExp(r'^(?=.*?[a-zA-Z0-9!@#\$&*~]).{8,16}$');
 
-  // 조건에 맞는지 검사
-  return pattern.hasMatch(password);
+    // 조건에 맞는지 검사
+    return pattern.hasMatch(this);
+  }
+
+  /// 닉네임 검증
+  bool isNickName() {
+    return RegExp(r'^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,12}$').hasMatch(this);
+  }
 }
