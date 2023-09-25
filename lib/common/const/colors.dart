710d87f57d9a5dd2d75e07a5d227f2ed18a844b2
김유나 <yuna000804@gmail.com>
2023-09-26 00:39:21 +0900

    fix: 공통 Colors 변수명 2차 수정

--- lib/common/const/colors.dart
+++ lib/common/const/colors.dart
@@ -5,26 +5,26 @@ const Color kColorBiskitNavy = Color(0xFF0C1142);
 // Background Color
 const Color kColorBgPrimaryWeak = Color(0xfffdfade);
 const Color kColorBgPrimary = Color(0xfffef076);
-const Color kColorBgPrimaryStrong = Color(0xffffe333);
+const Color kColorBgPrimaryStrong = Color(0xffffe433);
 const Color kColorBgSecondary = Color(0xff838ded);
 const Color kColorBgDefault = Color(0xFFFFFFFF);
-const Color kColorBgElevation1 = Color(0xfff8f9fb);
+const Color kColorBgElevation1 = Color(0xfff8fafc);
 const Color kColorBgElevation2 = Color(0xfff1f4f9);
 const Color kColorBgElevation3 = Color(0xffe4e9f1);
 const Color kColorBgInverseWeak = Color(0xff465063);
 const Color kColorBgInverse = Color(0xff141b2e);
-const Color kColorBgError = Color(0xfffdefef);
+const Color kColorBgError = Color(0xfffdf0f0);
 const Color kColorBgOverlay = Color(0xb2000000);
 const Color kColorBgDimmed = Color(0x7f000000);
 
 // Border Color
-const Color kColorBorderPrimary = Color(0xffffe333);
+const Color kColorBorderPrimary = Color(0xffffe433);
 const Color kColorBorderPrimaryStrong = Color(0xffffd01c);
 const Color kColorBorderDefalut = Color(0xffe4e9f1);
 const Color kColorBorderStrong = Color(0xffd3d9e2);
 const Color kColorBorderStronger = Color(0xff677389);
 const Color kColorBorderOnBgInverse = Color(0xff465063);
-const Color kColorBorderError = Color(0xffde2b22);
+const Color kColorBorderError = Color(0xffdf2c22);
 
 // Content Color
 const Color kColorContentDefault = Color(0xff141b2e);
@@ -32,11 +32,11 @@ const Color kColorContentOnBgPrimary = Color(0xff141b2e);
 const Color kColorContentWeak = Color(0xff465063);
 const Color kColorContentWeaker = Color(0xff677389);
 const Color kColorContentWeakest = Color(0xff9ca6b8);
-const Color kColorContentDisabled = Color(0xffbbc3cf);
-const Color kColorContentPlaceholder = Color(0xffbbc3cf);
-const Color kColorContentInverseWeak = Color(0xfff8f9fb);
+const Color kColorContentDisabled = Color(0xffbcc4cf);
+const Color kColorContentPlaceholder = Color(0xffbcc4cf);
+const Color kColorContentInverseWeak = Color(0xfff8fafc);
 const Color kColorContentInverse = Color(0xffffffff);
 const Color kColorContentPrimary = Color(0xfff9b301);
 const Color kColorContentSecondary = Color(0xff838ded);
-const Color kColorContentSeccess = Color(0xff009939);
-const Color kColorContentError = Color(0xffde2b22);
+const Color kColorContentSeccess = Color(0xff00993a);
+const Color kColorContentError = Color(0xffdf2c22);
