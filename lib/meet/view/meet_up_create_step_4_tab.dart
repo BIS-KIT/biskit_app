557faaea67e8edef344d0a664fb1fd98567b6036
김유나 <yuna000804@gmail.com>
2023-11-04 23:44:25 +0900

    fix: 모임생성 step 3, step 4 키보드 올라올 때 overflow 에러 해결

--- lib/meet/view/meet_up_create_step_4_tab.dart
+++ lib/meet/view/meet_up_create_step_4_tab.dart
@@ -57,241 +57,245 @@ class _MeetUpCreateStep4TabState extends ConsumerState<MeetUpCreateStep4Tab> {
   Widget build(BuildContext context) {
     final createMeetUpState = ref.watch(createMeetUpProvider);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 32,
-          ),
-          GestureDetector(
-            onTap: () {
-              showBiskitBottomSheet(
-                title: '',
-                customTitleWidget: Padding(
-                  padding: const EdgeInsets.only(
-                      top: 20, left: 20, right: 20, bottom: 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        width: 44,
-                        height: 44,
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            '아이콘 선택',
-                            style: getTsHeading18(context).copyWith(
-                              color: kColorContentDefault,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 32,
+            ),
+            GestureDetector(
+              onTap: () {
+                showBiskitBottomSheet(
+                  title: '',
+                  customTitleWidget: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20, left: 20, right: 20, bottom: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 44,
+                          height: 44,
+                        ),
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              '아이콘 선택',
+                              style: getTsHeading18(context).copyWith(
+                                color: kColorContentDefault,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: SvgPicture.asset(
-                            'assets/icons/ic_cancel_line_24.svg',
-                            width: 24,
-                            height: 24,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                context: context,
-                contentWidget: Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Wrap(
-                    children: [
-                      ...subjectList.map(
-                        (e) => GestureDetector(
+                        GestureDetector(
                           onTap: () {
-                            setState(() {
-                              selectedSubject = e;
-                            });
-                            context.pop();
+                            Navigator.pop(context);
                           },
                           child: Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: ThumbnailIconWidget(
-                              iconUrl: e['imgUrl'],
-                              isSelected:
-                                  selectedSubject['value'] == e['value'],
+                            padding: const EdgeInsets.all(10),
+                            child: SvgPicture.asset(
+                              'assets/icons/ic_cancel_line_24.svg',
+                              width: 24,
+                              height: 24,
                             ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                ThumbnailIconWidget(
-                  iconUrl: selectedSubject['value'] != null
-                      ? selectedSubject['imgUrl']
-                      : 'assets/icons/ic_restaurant_fill_48.svg',
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
-                    padding: const EdgeInsets.all(4),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: kColorBgInverseWeak,
-                    ),
-                    child: SvgPicture.asset(
-                      'assets/icons/ic_pencil_fill_24.svg',
-                      width: 16,
-                      height: 16,
+                        )
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '제목을 입력해주세요',
-                style: getTsHeading18(context).copyWith(
-                  color: kColorContentDefault,
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomTextFormField(
-                onChanged: (value) {
-                  ref.read(createMeetUpProvider.notifier).onChangedName(value);
-                },
-                errorText: (createMeetUpState != null &&
-                        createMeetUpState.name != null &&
-                        createMeetUpState.name!.length < 5)
-                    ? '5자 이상으로 입력해주세요'
-                    : null,
-                maxLength: 30,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          if (showMeetupDescription)
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  height: 144,
-                  margin: const EdgeInsets.only(top: 12),
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: kColorBgElevation1,
-                    borderRadius: const BorderRadius.all(Radius.circular(6)),
-                    border: Border.all(
-                      width: 1,
-                      color: meetupDescriptionFocusNode.hasFocus
-                          ? kColorBorderStronger
-                          : kColorBgElevation3,
+                  context: context,
+                  contentWidget: Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Wrap(
+                      children: [
+                        ...subjectList.map(
+                          (e) => GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedSubject = e;
+                              });
+                              context.pop();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: ThumbnailIconWidget(
+                                iconUrl: e['imgUrl'],
+                                isSelected:
+                                    selectedSubject['value'] == e['value'],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  child: TextFormField(
-                    onChanged: (value) {
-                      ref
-                          .read(createMeetUpProvider.notifier)
-                          .onChangedDescription(value);
-                    },
-                    onTap: () {
-                      meetupDescriptionFocusNode.requestFocus();
-                    },
-                    controller: meetupDescriptionController,
-                    focusNode: meetupDescriptionFocusNode,
-                    maxLines: null,
-                    minLines: null,
-                    decoration: InputDecoration(
-                      hintText: '모임설명을 입력해주세요',
-                      border: InputBorder.none,
-                      isDense: true,
-                      contentPadding: EdgeInsets.zero,
-                      hintStyle: getTsBody16Rg(context).copyWith(
-                        color: kColorContentPlaceholder,
+                );
+              },
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  ThumbnailIconWidget(
+                    iconUrl: selectedSubject['value'] != null
+                        ? selectedSubject['imgUrl']
+                        : 'assets/icons/ic_restaurant_fill_48.svg',
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: kColorBgInverseWeak,
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/icons/ic_pencil_fill_24.svg',
+                        width: 16,
+                        height: 16,
                       ),
                     ),
-                    style: kTsEnBody16Rg.copyWith(
-                      color: kColorContentWeak,
-                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '제목을 입력해주세요',
+                  style: getTsHeading18(context).copyWith(
+                    color: kColorContentDefault,
                   ),
                 ),
                 const SizedBox(
-                  height: 4,
+                  height: 16,
                 ),
-                Text(
-                  '${meetupDescriptionController.text.length}/500',
-                  style: getTsCaption12Rg(context).copyWith(
-                    color: kColorContentWeakest,
+                CustomTextFormField(
+                  onChanged: (value) {
+                    ref
+                        .read(createMeetUpProvider.notifier)
+                        .onChangedName(value);
+                  },
+                  errorText: (createMeetUpState != null &&
+                          createMeetUpState.name != null &&
+                          createMeetUpState.name!.length < 5)
+                      ? '5자 이상으로 입력해주세요'
+                      : null,
+                  maxLength: 30,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            if (showMeetupDescription)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 144,
+                    margin: const EdgeInsets.only(top: 12),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: kColorBgElevation1,
+                      borderRadius: const BorderRadius.all(Radius.circular(6)),
+                      border: Border.all(
+                        width: 1,
+                        color: meetupDescriptionFocusNode.hasFocus
+                            ? kColorBorderStronger
+                            : kColorBgElevation3,
+                      ),
+                    ),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        ref
+                            .read(createMeetUpProvider.notifier)
+                            .onChangedDescription(value);
+                      },
+                      onTap: () {
+                        meetupDescriptionFocusNode.requestFocus();
+                      },
+                      controller: meetupDescriptionController,
+                      focusNode: meetupDescriptionFocusNode,
+                      maxLines: null,
+                      minLines: null,
+                      decoration: InputDecoration(
+                        hintText: '모임설명을 입력해주세요',
+                        border: InputBorder.none,
+                        isDense: true,
+                        contentPadding: EdgeInsets.zero,
+                        hintStyle: getTsBody16Rg(context).copyWith(
+                          color: kColorContentPlaceholder,
+                        ),
+                      ),
+                      style: kTsEnBody16Rg.copyWith(
+                        color: kColorContentWeak,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    '${meetupDescriptionController.text.length}/500',
+                    style: getTsCaption12Rg(context).copyWith(
+                      color: kColorContentWeakest,
+                    ),
+                  ),
+                ],
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (!showMeetupDescription) {
+                        showMeetupDescription = true;
+                        buttonText = '모임설명 삭제';
+                      } else {
+                        showConfirmModal(
+                          context: context,
+                          leftCall: () {
+                            Navigator.pop(context);
+                          },
+                          leftButton: '취소',
+                          rightCall: () {
+                            ref
+                                .read(createMeetUpProvider.notifier)
+                                .onChangedDescription('');
+                            setState(() {
+                              showMeetupDescription = false;
+                              buttonText = '모임설명 추가';
+                            });
+                            Navigator.pop(context);
+                          },
+                          rightButton: '삭제',
+                          rightBackgroundColor: kColorBgError,
+                          rightTextColor: kColorContentError,
+                          title: '모임 설명을 삭제하시겠어요?',
+                        );
+                      }
+                    });
+                  },
+                  child: OutlinedButtonWidget(
+                    text: buttonText,
+                    leftIconPath: !showMeetupDescription
+                        ? 'assets/icons/ic_plus_line_24.svg'
+                        : 'assets/icons/ic_cancel_line_24.svg',
+                    isEnable: true,
                   ),
                 ),
               ],
             ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (!showMeetupDescription) {
-                      showMeetupDescription = true;
-                      buttonText = '모임설명 삭제';
-                    } else {
-                      showConfirmModal(
-                        context: context,
-                        leftCall: () {
-                          Navigator.pop(context);
-                        },
-                        leftButton: '취소',
-                        rightCall: () {
-                          ref
-                              .read(createMeetUpProvider.notifier)
-                              .onChangedDescription('');
-                          setState(() {
-                            showMeetupDescription = false;
-                            buttonText = '모임설명 추가';
-                          });
-                          Navigator.pop(context);
-                        },
-                        rightButton: '삭제',
-                        rightBackgroundColor: kColorBgError,
-                        rightTextColor: kColorContentError,
-                        title: '모임 설명을 삭제하시겠어요?',
-                      );
-                    }
-                  });
-                },
-                child: OutlinedButtonWidget(
-                  text: buttonText,
-                  leftIconPath: !showMeetupDescription
-                      ? 'assets/icons/ic_plus_line_24.svg'
-                      : 'assets/icons/ic_cancel_line_24.svg',
-                  isEnable: true,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
