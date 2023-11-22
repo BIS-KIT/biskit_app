be6eac9c2bb6b5c851a2c93c9df74e3327025e6d
김유나 <yuna000804@gmail.com>
2023-11-23 03:59:14 +0900

    feat: 설정 페이지 화면 setting 폴더 view로 이동

---
+++ lib/setting/view/account_delete_step_1_screen.dart
@@ -0,0 +1,88 @@
+import 'package:biskit_app/common/const/fonts.dart';
+import 'package:biskit_app/common/layout/default_layout.dart';
+import 'package:biskit_app/setting/view/account_delete_step_2_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:biskit_app/common/const/colors.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AccountDeleteStep1Screen extends ConsumerStatefulWidget {
+  const AccountDeleteStep1Screen({
+    super.key,
+  });
+
+  @override
+  ConsumerState<AccountDeleteStep1Screen> createState() =>
+      _MeetUpCreateStep1TabState();
+}
+
+class _MeetUpCreateStep1TabState
+    extends ConsumerState<AccountDeleteStep1Screen> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> reasons = [
+      "탈퇴사유 1",
+      "탈퇴사유 2",
+      "탈퇴사유 3",
+      "탈퇴사유 4",
+      "탈퇴사유 5"
+    ];
+    return DefaultLayout(
+      title: '계정 삭제',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            child: Text(
+              "계정을 삭제하시려는\n이유가 무엇인가요?",
+              style: getTsHeading20(context).copyWith(
+                color: kColorContentDefault,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: reasons.length,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  height: 56,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AccountDeleteStep2Screen(
+                            accountDeleteReason: reasons[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Row(
+                        children: [
+                          Expanded(child: Text(reasons[index])),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          SvgPicture.asset(
+                            'assets/icons/ic_chevron_right_line_24.svg',
+                            colorFilter: const ColorFilter.mode(
+                              kColorContentWeaker,
+                              BlendMode.srcIn,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/setting/view/announcement_screen.dart
@@ -0,0 +1,107 @@
+import 'package:biskit_app/common/const/colors.dart';
+import 'package:biskit_app/common/const/fonts.dart';
+import 'package:biskit_app/common/layout/default_layout.dart';
+import 'package:biskit_app/user/model/user_model.dart';
+import 'package:biskit_app/user/provider/user_me_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AnnouncementScreen extends ConsumerStatefulWidget {
+  const AnnouncementScreen({super.key});
+
+  @override
+  ConsumerState<AnnouncementScreen> createState() => _AnnouncementScreenState();
+}
+
+class _AnnouncementScreenState extends ConsumerState<AnnouncementScreen> {
+  @override
+  Widget build(BuildContext context) {
+    bool isAdmin = (ref.watch(userMeProvider) as UserModel).is_admin;
+    return DefaultLayout(
+        title: '공지사항',
+        shape: const Border(
+          bottom: BorderSide(
+            width: 1,
+            color: kColorBorderDefalut,
+          ),
+        ),
+        actions: [
+          if (isAdmin)
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: 44,
+                height: 44,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(right: 10),
+                child: SvgPicture.asset(
+                  'assets/icons/ic_plus_line_24.svg',
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+            )
+          else
+            Container(),
+        ],
+        child: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 1,
+                    color: kColorBorderWeak,
+                  ),
+                ),
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '이달의 소셜링 선정',
+                              style: getTsBody16Rg(context)
+                                  .copyWith(color: kColorContentWeak),
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              '2023.10.31',
+                              style: getTsBody14Rg(context)
+                                  .copyWith(color: kColorContentWeakest),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      SvgPicture.asset(
+                        'assets/icons/ic_chevron_right_line_24.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: const ColorFilter.mode(
+                          kColorContentWeakest,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
+  }
+}
