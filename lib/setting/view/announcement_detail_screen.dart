2756b9f7e7ba9c12401c42d29e98fcb8522be3c9
김유나 <yuna000804@gmail.com>
2023-11-28 04:39:54 +0900

    feat: 공지사항 detail 페이지 공지 수정, 삭제 구현

--- lib/setting/view/announcement_detail_screen.dart
+++ lib/setting/view/announcement_detail_screen.dart
@@ -1,11 +1,19 @@
 import 'package:biskit_app/common/const/colors.dart';
 import 'package:biskit_app/common/const/fonts.dart';
 import 'package:biskit_app/common/layout/default_layout.dart';
+import 'package:biskit_app/common/utils/widget_util.dart';
 import 'package:biskit_app/setting/model/notice_model.dart';
+import 'package:biskit_app/setting/repository/setting_repository.dart';
+import 'package:biskit_app/setting/view/write_announcement_screen.dart';
+import 'package:biskit_app/user/model/user_model.dart';
+import 'package:biskit_app/user/provider/user_me_provider.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class AnnouncementDetailScreen extends StatefulWidget {
+class AnnouncementDetailScreen extends ConsumerStatefulWidget {
   final NoticeModel notice;
   const AnnouncementDetailScreen({
     Key? key,
@@ -13,15 +21,56 @@ class AnnouncementDetailScreen extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AnnouncementDetailScreen> createState() =>
+  ConsumerState<AnnouncementDetailScreen> createState() =>
       _AnnouncementDetailScreenState();
 }
 
-class _AnnouncementDetailScreenState extends State<AnnouncementDetailScreen> {
+class _AnnouncementDetailScreenState
+    extends ConsumerState<AnnouncementDetailScreen> {
   final DateFormat dateFormat = DateFormat('yyyy.MM.dd', 'ko');
+  deleteNotice() async {
+    await ref.read(settingRepositoryProvider).deleteNotice(
+        notice_id: widget.notice.id, user_id: widget.notice.user.id);
+  }
+
+  onTapMore() {
+    showMoreBottomSheet(
+      context: context,
+      list: [
+        MoreButton(
+            text: '수정하기',
+            color: kColorContentDefault,
+            onTap: () async {
+              bool isBack = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WriteAnnouncementScreen(
+                    isEditMode: true,
+                    notice: widget.notice,
+                  ),
+                ),
+              );
+              if (isBack && mounted) {
+                Navigator.pop(context);
+                Navigator.pop(context, true);
+              }
+            }),
+        MoreButton(
+          text: '삭제하기',
+          color: kColorContentError,
+          onTap: () {
+            deleteNotice();
+            Navigator.pop(context);
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    bool isAdmin = (ref.watch(userMeProvider) as UserModel).is_admin;
     return DefaultLayout(
       title: '공지사항',
       shape: const Border(
@@ -30,51 +79,77 @@ class _AnnouncementDetailScreenState extends State<AnnouncementDetailScreen> {
           color: kColorBorderDefalut,
         ),
       ),
+      actions: [
+        if (isAdmin)
+          GestureDetector(
+            onTap: () {
+              onTapMore();
+            },
+            child: Container(
+              width: 44,
+              height: 44,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(right: 10),
+              child: SvgPicture.asset(
+                'assets/icons/ic_more_horiz_line_24.svg',
+                width: 24,
+                height: 24,
+              ),
+            ),
+          )
+        else
+          Container(),
+      ],
       backgroundColor: kColorBgElevation1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            alignment: Alignment.topLeft,
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  width: 1,
-                  color: kColorBorderDefalut,
+      child: Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1,
+                      color: kColorBorderDefalut,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.notice.title,
+                        style: getTsHeading18(context)
+                            .copyWith(color: kColorContentWeak),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        dateFormat
+                            .format(DateTime.parse(widget.notice.created_time)),
+                        style: getTsBody14Rg(context)
+                            .copyWith(color: kColorContentWeakest),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(
-                    widget.notice.title,
-                    style: getTsHeading18(context)
-                        .copyWith(color: kColorContentWeak),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    dateFormat
-                        .format(DateTime.parse(widget.notice.created_time)),
-                    style: getTsBody14Rg(context)
-                        .copyWith(color: kColorContentWeakest),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  widget.notice.content,
+                  style:
+                      getTsBody16Rg(context).copyWith(color: kColorContentWeak),
+                ),
               ),
-            ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text(
-              widget.notice.content,
-              style: getTsBody16Rg(context).copyWith(color: kColorContentWeak),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
