32e857d8ee3743572fbe66a1e443d6096d6332be
dddog <azskw1101@gmail.com>
2023-12-12 01:50:58 +0900

    fix : 알림 설정에 따라 푸시알림 받기 처리

--- lib/common/utils/local_notification_util.dart
+++ lib/common/utils/local_notification_util.dart
@@ -1,4 +1,3 @@
-import 'package:biskit_app/common/const/data.dart';
 import 'package:biskit_app/common/utils/logger_util.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -23,6 +22,9 @@ localNotificationInit() async {
 
   await notifications.initialize(
     initializationSettings,
+    onDidReceiveNotificationResponse: (details) {
+      logger.d(details);
+    },
   );
 }
 
@@ -44,12 +46,6 @@ showNotification(
     presentBadge: true,
     presentSound: true,
   );
-
-  logger.d(notifications.toString());
-
-  // if (!criticalNotification && !generalNotification) {
-  //   return;
-  // }
 
   // 알림 id, 제목, 내용 맘대로 채우기
   notifications.show(
