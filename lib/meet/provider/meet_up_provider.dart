aa68957a3e791829b7dea63f0a731d3d014b09c9
김유나 <yuna000804@gmail.com>
2024-07-09 21:46:26 +0900

    fix: 모임 탐색 페이지 paginate에 is_public 파라미터로 전달 대신 provider 값 수정으로 변경

--- lib/meet/provider/meet_up_provider.dart
+++ lib/meet/provider/meet_up_provider.dart
@@ -1,5 +1,7 @@
 import 'package:biskit_app/common/model/cursor_pagination_model.dart';
 import 'package:biskit_app/common/provider/pagination_provider.dart';
+import 'package:biskit_app/common/provider/root_provider.dart';
+import 'package:biskit_app/common/utils/logger_util.dart';
 import 'package:biskit_app/meet/model/meet_up_model.dart';
 import 'package:biskit_app/meet/provider/meet_up_filter_provider.dart';
 import 'package:biskit_app/meet/repository/meet_up_repository.dart';
@@ -35,17 +37,19 @@ class MeetUpStateNotifier
     Object? filter,
     bool? isPublic,
   }) async {
+    final rootState = ref.watch(rootProvider);
+
     isLoading = true;
     orderBy = orderBy;
     filter = ref.read(meetUpFilterProvider).filterGroupList;
-
+    logger.d('isPublic-paginate: ${rootState.isPublic}');
     filter = super.paginate(
       fetchCount: fetchCount,
       fetchMore: fetchMore,
       forceRefetch: forceRefetch,
       orderBy: orderBy,
       filter: filter,
-      isPublic: isPublic,
+      isPublic: rootState.isPublic ?? false,
     );
     isLoading = false;
   }
--- lib/meet/view/meet_up_list_screen.dart
+++ lib/meet/view/meet_up_list_screen.dart
@@ -3,7 +3,6 @@ import 'package:biskit_app/common/components/pagination_list_view.dart';
 import 'package:biskit_app/common/const/colors.dart';
 import 'package:biskit_app/common/const/fonts.dart';
 import 'package:biskit_app/common/provider/root_provider.dart';
-import 'package:biskit_app/common/utils/logger_util.dart';
 import 'package:biskit_app/common/utils/widget_util.dart';
 import 'package:biskit_app/meet/components/meet_up_card_widget.dart';
 import 'package:biskit_app/meet/components/meet_up_filter_sheet_widget.dart';
@@ -226,10 +225,9 @@ class _MeetUpListScreenState extends ConsumerState<MeetUpListScreen>
         ],
         onTap: (value) async {
           context.loaderOverlay.show();
-          logger.d('isPublic-index ${tabController.index}');
-          await ref.read(meetUpProvider.notifier).paginate(
-                isPublic: tabController.index == 1,
-              );
+          ref
+              .read(rootProvider.notifier)
+              .onSelectPublic(tabController.index == 1);
           if (!mounted) return;
           context.loaderOverlay.hide();
         },
@@ -337,7 +335,6 @@ class _MeetUpListScreenState extends ConsumerState<MeetUpListScreen>
   }
 
   Expanded _buildList(BuildContext context, MeetUpState filterState) {
-    logger.d('aaaa ${tabController.index}');
     return Expanded(
       child: PaginationListView(
         isPublic: tabController.index == 1,
